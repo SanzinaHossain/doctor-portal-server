500996ef3c515105d0406236df2607be61830b6c
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-18 12:18:41 +0600

    database connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors=require('cors')
 require('dotenv').config()
 const app = express()
@@ -6,6 +7,29 @@ const port = process.env.PORT || 5000
 //middle wire for cors
 app.use(cors())
 app.use(express.json())
+
+//database connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnwni.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ async function run(){
+   try{
+         await client.connect();
+         const serviceCollection=client.db('doctor-portal').collection('services');
+         console.log("database connected")
+
+         //get all data from services
+         app.get('/services',async(req,res)=>{
+           const query={};
+           const cursor=serviceCollection.find(query);
+           const services=await cursor.toArray()
+           res.send(services)
+         })
+   }
+   finally{
+
+   }
+ }
+ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
