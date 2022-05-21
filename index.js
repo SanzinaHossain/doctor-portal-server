a44d6285eb4bd5e2ca027f84bb3a4d0b6afe2809
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-22 01:01:01 +0600

    appointment done

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
    try{
          await client.connect();
          const serviceCollection=client.db('doctor-portal').collection('services');
+         const bookingCollection=client.db('doctor-portal').collection('booking');
          console.log("database connected")
 
          //get all data from services
@@ -22,6 +23,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
            const query={};
            const cursor=serviceCollection.find(query);
            const services=await cursor.toArray()
+           res.send(services)
+         })
+         //send booking data from client to server
+         app.post('/booking',async(req,res)=>{
+              const booking=req.body;
+              const query={treatment:booking.treatment,date:booking.date,PatientEmail:booking.PatientEmail}
+              const have=await bookingCollection.findOne(query)
+              if(have)
+              {
+                return res.send({success:false,booking:have})
+              }
+             else{
+              const result=await bookingCollection.insertOne(booking);
+              return res.send({success:true,result});
+             }
+         })
+         //This is not proper way
+         //aggregition pipeline use as an expert
+         app.get('/available',async(req,res)=>{
+           const date=req.query.date;
+           //get all services
+           const services=await serviceCollection.find().toArray();
+           //get the booking of that date
+           const query={date:date};
+           const booking=await bookingCollection.find(query).toArray();
+           services.forEach(service=>{
+             const serviceBooking=booking.filter(b=>b.treatment==service.name)
+             const booked=serviceBooking.map(s=>s.slot);
+             const available=service.slots.filter(s=>!booked.includes(s))
+             service.slots=available;
+           })
            res.send(services)
          })
    }
